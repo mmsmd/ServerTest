be2d1de59fc7aedc9d2a9bab52d4a76ee1a7d621
mmsmd <mao123488678@gmail.com>
2024-11-11 22:44:55 +0800

    优化BMC WEB的演示脚本

--- modules/web/home.py
+++ modules/web/home.py
@@ -1,12 +1,13 @@
 from selenium.webdriver.common.by import By
 from conftest import bmc_client
+from modules.common import load_nav_map
 from modules.web.nav import change_nav
 
 
-def get_home_product_name(bmc_client):
+def get_home_product_name(bmc_client, nav_name):
     """获取服务器类型"""
-    change_nav(bmc_client,"navHome")
-    home_product_name = bmc_client.driver.find_element(By.ID, "homeProductName").text
+    element_id = load_nav_map().get(nav_name.lower())
+    home_product_name = bmc_client.driver.find_element(By.ID, element_id).text
     return home_product_name
 
 
--- tests/BMC/test_bmc_product_name.py
+++ tests/BMC/test_bmc_product_name.py
@@ -2,10 +2,13 @@ import pytest
 
 from modules.common import check_values
 from modules.web.home import get_home_product_name
+from modules.web.nav import change_nav
+
 
 def test_bmc_product_name(bmc_client, logger):
     """测试 BMC 登录并获取主机名"""
     bmc_client.start_driver()
     bmc_client.login()
-    product_name = get_home_product_name(bmc_client)
+    change_nav(bmc_client, "nav_home")
+    product_name = get_home_product_name(bmc_client, "home_product_name")
     check_values("1288H V6", product_name, check_type="equal")
